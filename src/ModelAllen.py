28758629c711bf251c748c7322eeae3b91d68958
Allen <ab082004@gmail.com>
2015-06-10 01:10:05 +0800

    now it run in parallel!

--- src/ModelAllen.py
+++ src/ModelAllen.py
@@ -3,9 +3,11 @@ import numpy as np
 import json
 import sys
 import argparse
-from scipy.spatial.distance import cdist
+from multiprocessing import Pool
+import time
 
 def main(args):
+	timePoint1 = time.time()
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-t", type = str, default = './train.csv')
 	parser.add_argument("-i", type = str, default = './test.csv')
@@ -23,7 +25,7 @@ def main(args):
 	output = args.o
 	global digits
 	digits = args.d
-	global digits
+	global errorThreshold
 	errorThreshold = args.d
 
 	global polylines
@@ -43,24 +45,49 @@ def main(args):
 		if len(trajectoryData[-1])>26:
 			metadata.append(trajectoryData[:-1])
 
-			polyline = json.loads(trajectoryData[-1])
-			polylineBackUp = polyline[:]
-			lastNode = polyline[0]
-			for node in polyline:
-				if abs(node[0]-lastNode[0])+abs(node[1]-lastNode[1])<0.01: lastNode = node
-				else:
-					print lastNode
-					polylineBackUp.remove(node)
-			
-			polylines.append(np.around(polylineBackUp,digits))
+			polyline = np.around(json.loads(trajectoryData[-1]),digits)
+			errorHappen = False
+			for nodeNum in range(len(polyline)-1): #remove GPS error(error1)
+				if np.linalg.norm(polyline[nodeNum]-polyline[nodeNum+1])>0.01:
+					errorHappen = True
+					break
+
+			if errorHappen is False: #remove cycle polylines(error2)
+				midPoint = polyline[len(polyline)/2]
+				if (np.dot((midPoint-polyline[0]),(polyline[-1]-midPoint)) < 0): errorHappen = True
+
+			if errorHappen is True: polylines.append(polyline)
 	testTrajectoryFile = csv.reader(open(testFile,'r'))
 	next(testTrajectoryFile)
 	for testFileLine in testTrajectoryFile:
 		testMetadata.append(testFileLine[:-1])
 		testPolylineData.append(np.around(json.loads(testFileLine[-1]),digits))
+	timePoint2 = time.time()
 	setDestination()
+	timePoint3 = time.time()
 	buildAnswerFile()
+	timePoint4 = time.time()
+	print timePoint2 - timePoint1
+	print timePoint3 - timePoint2
+	print timePoint4 - timePoint3
 
+def buildAnswerFile():
+	File = open(output,'w')
+	File.write('TRIP_ID'+','+'LATITUDE'+','+'LONGITUDE'+'\n')
+	p = Pool()
+	answer = p.map(BayesMethod,testPolylineData)
+	for ans in xrange(len(answer)):
+		File.write(str(testMetadata[ans][0]) + ',' + str(answer[ans][1]) + ',' + str(answer[ans][0]) + '\n')	
+	File.close
+"""
+def buildAnswerFile():
+	File = open(output,'w')
+	File.write('TRIP_ID'+','+'LATITUDE'+','+'LONGITUDE'+'\n')
+	for ans in xrange(len(testPolylineData)):
+		answer = BayesMethod(testPolylineData[ans])
+		File.write(str(testMetadata[ans][0]) + ',' + str(answer[1]) + ',' + str(answer[0]) + '\n')
+	File.close
+"""
 def setDestination(): #(coordinate of the destinations,prob to reach this destinations,which polylines reach this destinations)
 	for polylineNum in xrange(len(polylines)):
 		isNew = True
@@ -71,8 +98,8 @@ def setDestination(): #(coordinate of the destinations,prob to reach this destin
 				break
 		if isNew == True: destinations.append([polylines[polylineNum][-1],[polylineNum]])
 
-def BayesMethod(testPolylineData,testMetadata):
-	scoreList = xrange(len(destinations))
+def BayesMethod(testPolylineData):
+	scoreList = range(len(destinations))
 	polylineSimilarityScore = polylineSimilarityScoring(testPolylineData)
 	#callTypeScore = callTypeScoring(testMetadata)
 	#dayTypescore = dayTypeScoring(testMetadata)
@@ -137,13 +164,5 @@ def dayTypeScoring(testMetadata):
 	for scoreNum in xrange(len(dayTypeSimilarityScore)): dayTypeSimilarityScore[scoreNum]/sumOfScore
 	return dayTypeSimilarityScore
 """
-
-def buildAnswerFile():
-	File = open(output,'w')
-	File.write('TRIP_ID'+','+'LATITUDE'+','+'LONGITUDE'+'\n')
-	for ans in xrange(len(testPolylineData)):
-		answer = BayesMethod(testPolylineData[ans],testMetadata[ans])
-		File.write(str(testMetadata[ans][0]) + ',' + str(answer[1]) + ',' + str(answer[0]) + '\n')
-	File.close
 
 main(sys.argv[1:])
