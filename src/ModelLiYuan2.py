f71ff5ef105adcf1a6ef588cc50906792c498765
nkfly <nkflyfly@gmail.com>
2015-06-19 15:43:45 +0800

    latest model

--- src/ModelLiYuan2.py
+++ src/ModelLiYuan2.py
@@ -4,44 +4,14 @@ import numpy
 import math
 from sklearn.ensemble import RandomForestClassifier
 import sys
-
-
-def distance_measure(point1, point2):
-	return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
-
-def polyline_statistics(file_name):
-
-	data = []
-	with open(file_name, 'r') as f:
-		f.readline()
-		for line in f:
-			entries = line.strip().split('"')
-			polylines = json.loads(entries[-2])
-
-			distance = 0.0
-			for i in range(len(polylines)-1 ):
-				distance += distance_measure(polylines[i], polylines[i+1])
-			data.append(distance)
-
-
-	return numpy.mean(data), numpy.std(data)
-
+import datetime
+import random
 
 
 def coordinateTransf(point, digits): #"[1.233,2.445]"=>[1.23,2.45]
 	result = str(round(point[0],digits))+str(round(point[1],digits))
 	return result
 
-def polylines_to_vector_representation(polylines, last_k_point):
-	vector_representation = []
-	vector_representation.extend(polylines[len(polylines)-last_k_point])
-
-	for i in range(len(polylines) - last_k_point , len(polylines) - 1):
-		vector_representation.append(polylines[i+1][0] - polylines[i][0])
-		vector_representation.append(polylines[i+1][1] - polylines[i][1])
-
-
-	return vector_representation
 
 
 def create_feature_vector(line, destination_to_label, label_to_point,point_num,train_keep_ratio):
@@ -60,6 +30,13 @@ def create_feature_vector(line, destination_to_label, label_to_point,point_num,t
 		label_to_point[destination_to_label[destination]] = [round(polylines[-1][0],digits),round(polylines[-1][1],digits)]
 
 	len_to_keep = int(len(polylines)*train_keep_ratio)
+
+	polylines = polylines[:len_to_keep]
+
+
+
+
+
 
 
 	feature_vector = []
@@ -102,6 +79,52 @@ def create_feature_vector_test(line, point_num):
 
 	return True, feature_vector
 
+def add_meta(line, feature_vector):
+	entries = line.strip().split(',')
+	call_type = entries[1].strip('"')
+
+	if call_type == 'A':
+		feature_vector.extend([1,0,0])
+	elif call_type == 'B':
+		feature_vector.extend([0,1,0])
+	else:
+		feature_vector.extend([0,0,1])
+
+	timestamp = entries[5].strip('"')
+	dtime = datetime.datetime.fromtimestamp(int(timestamp))
+	weekday = dtime.weekday()
+
+	for i in range(7):
+		if weekday == i:
+			feature_vector.append(1)
+		else:
+			feature_vector.append(0)
+
+	hour = dtime.hour
+	if hour >= 0 and hour <= 8:
+		feature_vector.extend([1,0,0,0])
+	elif hour <= 12:
+		feature_vector.extend([0,1,0,0])
+	elif hour <= 6:
+		feature_vector.extend([0,0,1,0])
+	else:
+		feature_vector.extend([0,0,0,1])
+
+
+	day_type = entries[6].strip('"')
+	if day_type == 'A':
+		feature_vector.extend([1,0,0])
+	elif day_type == 'B':
+		feature_vector.extend([0,1,0])
+	else:
+		feature_vector.extend([0,0,1])
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -113,59 +136,81 @@ if __name__ == '__main__':
 		exit(-1)
 
 
-	destination_to_label = {}
-	label_to_point = {}
+	
 	digits = 3 # 1:305, 2:2732, 3:24966, 4:249482, 5:1239996
 
+	# X = []
+	# y = []
+
+	point_num = 100
+	train_keep_ratio = 0.7792
+
+
+	answserDict = {}
 	X = []
 	y = []
-
-	point_num = 400
-	train_keep_ratio = 0.8
-
-
+	destination_to_label = {}
+	label_to_point = {}
 	with open(train_file, 'r') as f:
 		f.readline()
 		for line in f:
+			entries = line.strip().split(',')
+			timestamp = entries[5].strip('"')
+			dtime = datetime.datetime.fromtimestamp(int(timestamp))
+			weekday = dtime.weekday()
+
+
 			isOk, feature_vector, label = create_feature_vector(line, destination_to_label, label_to_point,point_num,train_keep_ratio)
 			if not isOk:
 				continue
 
+
+			add_meta(line, feature_vector)
 			X.append(feature_vector)
 			y.append(label)
 
 
-	print 'training ...'
-	clf = RandomForestClassifier(max_depth=15,n_estimators=100, n_jobs=20)
+
+	print 'training ... '
+	clf = RandomForestClassifier(max_depth=15,n_estimators=10, n_jobs=20)
 	# print len(X[1])
 	clf.fit(X, y)
 
 	X = []
 
 	print 'prediction ...'
+
 	with open(test_file, 'r') as f:
 		f.readline()
 		for line in f:
+			entries = line.strip().split(',')
+			timestamp = entries[5].strip('"')
+			dtime = datetime.datetime.fromtimestamp(int(timestamp))
+			weekday = dtime.weekday()
+
+
+
 			isOk, feature_vector = create_feature_vector_test(line, point_num)
 			if not isOk:
 				print 'dangerous'
 				continue
 
+			add_meta(line, feature_vector)
+
 			X.append(feature_vector)
 	# print len(X[1])
 	predictions = clf.predict(X)
 
-
 	index = 0
-	print len(label_to_point)
 	with open('answer.csv', 'w') as w:
 		w.write('TRIP_ID,LATITUDE,LONGITUDE\n')
 		with open('answer_sample.csv', 'r') as f:
 			f.readline()
 			for line in f:
 				entries = line.strip().split(',')
+				tid = entries[0].strip('"')
 				point =  label_to_point[predictions[index]]
-				# print predictions[index]
+				# print label_to_point[index]
 
 				w.write(entries[0] + ',' + str(point[1]) + ',' + str(point[0]) + '\n')
 				index += 1
